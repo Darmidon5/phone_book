fa98aa065b1e4ad78976c54610d6f9d598f8dcb0
Darmidon5 <niks_remez2@mail.ru>
2023-09-08 17:55:30 +0300

    trying to fix bug on Windows

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,27 +2,23 @@ import pytest
 import model
 import os
 
+
 @pytest.fixture
 def test_filename() -> str:
     dir_path: str = os.path.dirname(os.path.realpath(__file__))
-    test_filename = dir_path + f'/test_client_data.csv'
-    # is_file_exists function checks this variable with os.path.isabs
-    # if you violate its rules tests won't work
-    # read official documentation before changing test_filename
-
-    # also you should check test_controller
+    test_filename = os.path.join(dir_path, f'test_client_data.csv')
     return test_filename
 
 
 @pytest.fixture
 def test_phone_book():
-    test_clients_data = model.PhoneBookRepository(os.path.dirname(os.path.realpath(__file__)) + '/test_client_data.csv',
+    test_clients_data = model.PhoneBookRepository(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test_client_data.csv'),
                                              ['ФИО', 'название организации', 'рабочий телефон', 'сотовый телефон'], ';')
     return test_clients_data
 
 
 @pytest.fixture
 def test_sorted_phone_book():
-    test_sorted_clients_data = model.PhoneBookRepository(os.path.dirname(os.path.realpath(__file__)) + '/sorted_csv',
+    test_sorted_clients_data = model.PhoneBookRepository(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'sorted_csv'),
                                              ['ФИО', 'название организации', 'рабочий телефон', 'сотовый телефон'], ';')
     return test_sorted_clients_data
